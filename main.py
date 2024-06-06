053a408d602c37b35bd1fa45dde4d7e3c18f545b
Aryaman Sawhney <aryamansawhney1@gmail.com>
2024-06-05 23:24:35 -0400

    work

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import requests
 
-two = open("2.png", "rb")
-requests.put("http://127.0.0.1:8000/set_pfp?uid=0", data=two.read())
-two.close()
+url = "http://127.0.0.1:8000/set_pfp"
+params = {"uid": 0}
+files = {"pfp": ("0.png", open("0.png", "rb"), "image/png")}
+
+
+response = requests.put(url, params=params, files=files)
+
+for i in range(500):
+    resp = requests.get(f"http://127.0.0.1:8000/get_account?uid={i}")
+    if resp.status_code == 200:
+        print(resp.json())
+    else:
+        print(f"Error Code {resp.status_code}")
